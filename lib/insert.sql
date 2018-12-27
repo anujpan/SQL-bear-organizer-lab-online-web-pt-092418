015fb2cde1595bb42f81a15a0caa9434940f5f33
Anuj Panchal <anujpan1@gmail.com>
2018-12-27 21:01:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, null, null, null, null);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, null, null, null, null);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, null, null, null, null);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, null, null, null, null);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, null, null, null, null);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, null, null, null, null);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 1, null, null, null, null);
